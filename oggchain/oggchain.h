d73080863c410aa1c5aef248dee4e4367d7788b1
Kenneth Arnold <kcarnold@xiph.org>
2002-01-21 18:38:58 +0000

    First commit of the beginnings of oggchain. See email on list.
    svn path=/trunk/vorbis-tools/; revision=2969

---
+++ oggchain/oggchain.h
@@ -0,0 +1,17 @@
+/* oggchain v0.1
+     Hacked together by Kenneth Arnold <oggchain@arnoldnet.net>
+   
+   A simple tool to work with Ogg files as ordered archives. Currently
+   only works with Vorbis (because that's all vorbisfile supports),
+   but is designed with expandibility to all Ogg chained streams in
+   mind.
+*/
+
+#ifndef __OGGCHAIN_H__
+#define __OGGCHAIN_H__
+
+#include <ogg/ogg.h>
+#include <vorbis/vorbisfile.h>
+
+
+#endif /* __OGG_CHAIN_H_ */
