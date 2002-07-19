f366c87d7e1da8da97d1c5995c264db2755fa0e3
Ralph Giles <giles@xiph.org>
2002-07-19 08:44:52 +0000

    Protect utf8.h from multiple inclusion and C++ mangling to facilitate linking of vorbis-tools code into other projects. Patch from David Gasaway.
    svn path=/trunk/vorbis-tools/; revision=3681

--- include/utf8.h
+++ include/utf8.h
@@ -17,7 +17,20 @@
  *   3 : unknown encoding (but still converted, using '?')
  */
 
+#ifndef __UTF8_H
+#define __UTF8_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
 void convert_set_charset(const char *charset);
 
 int utf8_encode(const char *from, char **to);
 int utf8_decode(const char *from, char **to);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* __UTF8_H */
