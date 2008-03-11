4953106cea20743b9438306c72ee38f2fa9d787d
sping <sping@xiph.org>
2008-03-11 01:06:44 +0000

    Copy libogg too, fix debug/release mix-up
    svn path=/trunk/vorbis-tools/; revision=14571

--- win32/VS2005/copy_libs_here.cmd
+++ win32/VS2005/copy_libs_here.cmd
@@ -3,8 +3,12 @@ SET VORBIS_DIR=..\..\..\libvorbis\win32\VS2005
 SET VORBIS_DEBUG=%VORBIS_DIR%\debug
 SET VORBIS_RELEASE=%VORBIS_DIR%\release
 
+SET OGG_DIR=..\..\..\libogg\win32\VS2005\
+SET OGG_DEBUG=%OGG_DIR%\debug
+SET OGG_RELEASE=%OGG_DIR%\release
+
 SET FLAC_DEBUG=..\..\..\flac\obj\debug\lib
-SET FLAC_RELEASE=..\..\..\flac\obj\debug\lib
+SET FLAC_RELEASE=..\..\..\flac\obj\release\lib
 
 
 COPY %VORBIS_DEBUG%\libvorbis.dll debug\
@@ -13,5 +17,10 @@ COPY %VORBIS_RELEASE%\libvorbis.dll release\
 COPY %VORBIS_DEBUG%\libvorbisfile.dll debug\
 COPY %VORBIS_RELEASE%\libvorbisfile.dll release\
 
+COPY %OGG_DEBUG%\libogg.dll debug\
+COPY %OGG_RELEASE%\libogg.dll release\
+
 COPY %FLAC_DEBUG%\libFLAC_dynamic.dll debug\
 COPY %FLAC_RELEASE%\libFLAC_dynamic.dll release\
+
+PAUSE
