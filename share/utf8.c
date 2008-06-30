d5b241f41082a6147c5d01452dd2ddd63e1efab6
ivo <ivo@xiph.org>
2008-06-30 13:00:32 +0000

    Improve Unicode behavior in Windows.  Replaces GetConsoleCP() by CP_ACP as according to Peter Harris, "Current Visual Studio libraries assume CP_ACP for all output, regardless of the console code page."
    svn path=/trunk/vorbis-tools/; revision=15093

--- share/utf8.c
+++ share/utf8.c
@@ -186,7 +186,7 @@ int utf8_decode(const char *from, char **to)
         return -1;
     }
 
-    chars = WideCharToMultiByte(GetConsoleCP(), WC_COMPOSITECHECK, unicode,
+    chars = WideCharToMultiByte(CP_ACP, WC_COMPOSITECHECK, unicode,
             -1, NULL, 0, NULL, NULL);
 
     if(chars == 0)
@@ -204,7 +204,7 @@ int utf8_decode(const char *from, char **to)
         return -1;
     }
 
-    err = WideCharToMultiByte(GetConsoleCP(), WC_COMPOSITECHECK, unicode, 
+    err = WideCharToMultiByte(CP_ACP, WC_COMPOSITECHECK, unicode, 
             -1, *to, chars, NULL, NULL);
     if(err != chars)
     {
