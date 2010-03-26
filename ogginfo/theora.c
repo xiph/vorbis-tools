90ee3b371154c1476b659e26b72dc9bf80d5c184
Ralph Giles <giles@xiph.org>
2010-03-26 05:07:26 +0000

    Only clean the theora_comment struct if we're passed a non-NULL pointer.
    The implementation of theora_comment_clear() in ogginfo performed a
    NULL check before deferencing members to free() them, but zeroed the
    indicated memory regardless.
    
    svn path=/trunk/vorbis-tools/; revision=17072

--- ogginfo/theora.c
+++ ogginfo/theora.c
@@ -111,8 +111,8 @@ void theora_comment_clear(theora_comment *tc){
     if(tc->user_comments)_ogg_free(tc->user_comments);
     if(tc->comment_lengths)_ogg_free(tc->comment_lengths);
     if(tc->vendor)_ogg_free(tc->vendor);
+    memset(tc,0,sizeof(*tc));
   }
-  memset(tc,0,sizeof(*tc));
 }
 
 static int _theora_unpack_comment(theora_comment *tc, oggpack_buffer *opb){
